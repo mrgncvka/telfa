27c41b6c40849c94f10080d0bf1861fdb2da1926
yep606 <ilya.rogov.vladislav@gmail.com>
2020-06-11 02:15:10 +0300

    test

--- src/main/resources/static/js/example.js
+++ src/main/resources/static/js/example.js
@@ -3,17 +3,41 @@ const ig = require('./instagram');
 const { Telegraf } = require('telegraf')
 
 
-const bot = new Telegraf(process.env.TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', async (ctx) => {
-   await ctx.reply('Hey there');
+const bot = new Telegraf(process.env.TOKEN);
+let user = {
+    login: "",
+    password: "",
+};
+let ready = false;
+
+bot.start((ctx) => {
+    ready = !ready;
+    return ctx.reply("Hey, I'm Telfa! Send me your Instagram login and password, " +
+        "After all write /done")
+});
+bot.command("done", async ctx => {
 
     await ig.initialize();
 
-    await ig.login("***", "***");
+    await ig.login(user.login, user.password);
 
+    return ctx.reply("Testing connection")
 
 })
-bot.launch()
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('text', (ctx) => {
+
+    if(ready && user.login === "")
+        user.login = ctx.message.text;
+    else if(ready && user.password === "")
+        user.password = ctx.message.text;
+
+    return ctx.reply('👍')
+});
+
+
+bot.hears('hi', async (ctx) => {
+    await ctx.reply('Hey there');
+
+});
+bot.launch();
