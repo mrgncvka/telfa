e947fbe54f85a2621a67c2a12a74240fccdc9fd6
yep606 <ilya.rogov.vladislav@gmail.com>
2020-07-02 23:08:56 +0300

    back

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -3,6 +3,10 @@ package base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * @author yep606
+ */
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
