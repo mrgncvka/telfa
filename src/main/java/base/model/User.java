ed26882eacfa802e5f9a53419e90ba6cf597a2b2
yep606 <ilya.rogov.vladislav@gmail.com>
2020-07-03 21:55:42 +0300

    back

--- src/main/java/base/model/User.java
+++ src/main/java/base/model/User.java
@@ -2,23 +2,23 @@ package base.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "usr")
-@Data
-public class User {
-
-    @Id
-    private Long id;
-    private String firstName;
-    private String authPassword;
-    private boolean bot;
-
-    private String token;
-    private String username;
-    private String instPassword;
-
-
-
-}
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "usr")
+//@Data
+//public class User {
+//
+//    @Id
+//    private Long id;
+//    private String firstName;
+//    private String authPassword;
+//    private boolean bot;
+//
+//    private String token;
+//    private String username;
+//    private String instPassword;
+//
+//
+//
+//}
--- src/main/java/base/repo/UserRepo.java
+++ src/main/java/base/repo/UserRepo.java
@@ -1,7 +1,7 @@
 package base.repo;
 
-import base.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
+//import base.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepo extends JpaRepository<User, Long> {
+//}
