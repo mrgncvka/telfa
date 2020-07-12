923f980b482fca3c5799498fa9bf0044dd5f6bc3
yep606 <ilya.rogov.vladislav@gmail.com>
2020-07-12 23:30:00 +0300

    fix

--- src/main/java/base/config/WebSecurityConfig.java
+++ src/main/java/base/config/WebSecurityConfig.java
@@ -1,4 +1,20 @@
 package base.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.security.Principal;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
 }
--- src/main/java/base/controller/MainController.java
+++ src/main/java/base/controller/MainController.java
@@ -1,38 +1,38 @@
-//package base.controller;
-//
-//import base.model.User;
-//import base.repo.UserRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping("/user")
-//public class MainController {
-//
-//    private final UserRepo userRepo;
-//
-//    @Autowired
-//    public MainController(UserRepo userRepo) {
-//        this.userRepo = userRepo;
-//    }
-//
-//    @PostMapping("/add")
-//    public User add(@RequestBody User user){
-//        return userRepo.save(user);
-//    }
-//
-//    @GetMapping("/id/{id}")
-//    public User getUser(@PathVariable Long id){
-//        Optional<User> user = userRepo.findById(id);
-//        return user.orElse(null);
-//    }
-//
-//    @GetMapping("/hello")
-//    public String test(){
-//        return "Hello!";
-//    }
-//
-//
-//}
+package base.controller;
+
+import base.model.User;
+import base.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class MainController {
+
+    private final UserRepo userRepo;
+
+    @Autowired
+    public MainController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @PostMapping("/add")
+    public User add(@RequestBody User user){
+        return userRepo.save(user);
+    }
+
+    @GetMapping("/id/{id}")
+    public User getUser(@PathVariable Long id){
+        Optional<User> user = userRepo.findById(id);
+        return user.orElse(null);
+    }
+
+    @GetMapping("/hello")
+    public String test(){
+        return "Hello!";
+    }
+
+
+}
